19e7e5997de87ade62325d4b38a642ac1cb8d4b6
infinity-plus <navdhareharsh2001@gmail.com>
2020-12-01 10:25:24 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const mockUserData = [
     { name: 'Mark' },
     { name: 'Jill' }
@@ -13,7 +16,7 @@ app.get('/users', function (req, res) {
     })
 })
 
-app.get('/users/:id',function(req, res) {
+app.get('/users/:id', function (req, res) {
     console.log(req.params.id)
     res.json({
         success: true,
@@ -21,6 +24,28 @@ app.get('/users/:id',function(req, res) {
         user: req.params.id
     })
 })
+
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username matched',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+}
+)
 
 app.listen(8000, function () {
     console.log("Server is running")
